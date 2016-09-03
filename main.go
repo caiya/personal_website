5f2946de7b78e2676a39e3a80c439ba9bf2ced04
caiya928 <caiya928@aliyun.com>
2016-09-03 18:08:04 +0800

    添加文件

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	_ "easyui_crud/models"
 	_ "personal_website/routers"
 
 	"github.com/astaxie/beego"
 )
 
+const VERSION = "1.0.0"
+
 func main() {
+	beego.AppConfig.Set("version", VERSION)
 	beego.Run()
 }
