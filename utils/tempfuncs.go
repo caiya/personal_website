00450533343d696a5f4418f94a0d86ee6bada5cc
晁州 <chaozhou@steelv.cn>
2016-09-18 15:21:17 +0800

    添加格式化时间戳方法

--- utils/tempfuncs.go
+++ utils/tempfuncs.go
@@ -1,22 +1,29 @@
 package utils
 
 import (
+	"time"
+
 	"github.com/astaxie/beego"
 )
 
 //加added
 func Up(in int, uped int) (out int) {
-	out = in + uped
-	return
+	return in + uped
 }
 
 //减added
 func Down(in int, downed int) (out int) {
-	out = in - downed
+	return in - downed
+}
+
+//格式化时间戳
+func FormatTimeStamp(timestamp int, layout string) (out string) {
+	out = time.Unix(int64(timestamp), 0).Format(layout)
 	return
 }
 
 func init() {
 	beego.AddFuncMap("Up", Up)
 	beego.AddFuncMap("Down", Down)
+	beego.AddFuncMap("FormatTimeStamp", FormatTimeStamp)
 }
