e06b09499d18092a22f71abfa4068b5601fb7c86
晁州 <chaozhou@steelv.cn>
2016-09-18 17:55:38 +0800

    修改路由备注

--- routers/router.go
+++ routers/router.go
@@ -14,37 +14,48 @@ func init() {
 	beego.Router("/admin/exit", &admin.IndexHandle{}, "GET:Exit")
 	beego.Router("/admin/main", &admin.MainHandle{}, "GET:Main")
 
-	//跳转路由
+	//系统设置
 	beego.Router("/admin/setting", &admin.SettingHandle{}, "GET:Index")
 
+	//单页模板
 	beego.Router("/admin/template", &admin.TemplateHandle{}, "GET:Index")
 	beego.Router("/admin/template/add", &admin.TemplateHandle{}, "GET:ToAdd")
 
+	//相册分类
 	beego.Router("/admin/gallerytype", &admin.GallerytypeHandle{}, "GET:Index")
 	beego.Router("/admin/gallerytype/add", &admin.GallerytypeHandle{}, "GET:ToAdd")
 
+	//相册
 	beego.Router("/admin/gallery", &admin.GalleryHandle{}, "GET:Index")
 	beego.Router("/admin/gallery/add", &admin.GalleryHandle{}, "GET:ToAdd")
 
+	//博客类别
 	beego.Router("/admin/articletype", &admin.ArticletypeHandle{}, "GET:Index")
 	beego.Router("/admin/articletype/add", &admin.ArticletypeHandle{}, "GET:ToAdd")
 
+	//文章|博客
 	beego.Router("/admin/article", &admin.ArticleHandle{}, "GET:Index")
 	beego.Router("/admin/article/add", &admin.ArticleHandle{}, "GET:ToAdd")
 
+	//评论
 	beego.Router("/admin/comment", &admin.CommentHandle{}, "GET:Index")
 
+	//后台管理员
 	beego.Router("/admin/manager", &admin.ManagerHandle{}, "GET:Index")
 	beego.Router("/admin/manager/update", &admin.ManagerHandle{}, "GET:Update")
 	beego.Router("/admin/manager/add", &admin.ManagerHandle{}, "GET:ToAdd")
 	beego.Router("/admin/manager/list", &admin.ManagerHandle{}, "POST:GetList")
 
+	//日志模块
 	beego.Router("/admin/log/?:page", &admin.RecordHandle{}, "*:Index")
 
+	//链接模块
 	beego.Router("/admin/link/delete/?:id", &admin.LinkHandle{}, "GET:Delete")
 	beego.Router("/admin/link/delete", &admin.LinkHandle{}, "POST:Del")
 	beego.Router("/admin/link/?:page", &admin.LinkHandle{}, "*:Index")
 	beego.Router("/admin/link/add", &admin.LinkHandle{}, "GET:ToAdd")
 	beego.Router("/admin/link/add", &admin.LinkHandle{}, "POST:Add")
+	beego.Router("/admin/link/update/?:id", &admin.LinkHandle{}, "GET:ToUpdate")
+	beego.Router("/admin/link/update/?:id", &admin.LinkHandle{}, "POST:Update")
 
 }
