e4b1d9dd7c0e48a7d65074cb468d30a843e421d0
晁州 <chaozhou@steelv.cn>
2016-09-29 15:47:27 +0800

    添加文章列表页查询

--- controllers/admin/article.go
+++ controllers/admin/article.go
@@ -1,7 +1,10 @@
 package admin
 
 import (
+	"fmt"
 	"personal_website/controllers"
+	"personal_website/models"
+	"strconv"
 )
 
 type ArticleHandle struct {
@@ -10,6 +13,51 @@ type ArticleHandle struct {
 
 //博客首页
 func (this *ArticleHandle) Index() {
+	id := this.GetString("id")
+	name := this.GetString("name")
+	url := this.GetString("url")
+
+	query := make(map[string]string, 0)
+
+	if id != "" {
+		query["id"] = id
+	}
+	if name != "" {
+		query["name.contains"] = name
+	}
+	if url != "" {
+		query["url.contains"] = url
+	}
+
+	page, _ := strconv.Atoi(this.Ctx.Input.Param(":page"))
+	if page == 0 {
+		page = 1
+	}
+	article := &models.Article{}
+
+	articles, count, err := article.GetList(query, nil, []string{"id"}, []string{"desc"}, int64(page), PAGESIZE)
+	if err == nil {
+		this.Data["datas"] = articles //记录数
+		this.Data["count"] = count    //总记录数
+	} else {
+		this.Data["datas"] = nil
+		this.Data["count"] = 0
+	}
+	var pageCount int64
+	if count%PAGESIZE == 0 {
+		pageCount = count / PAGESIZE
+	} else {
+		pageCount = count/PAGESIZE + 1
+	}
+
+	fmt.Println(articles)
+
+	//数据回显
+	this.Data["id"] = id
+	this.Data["name"] = name
+	this.Data["url"] = url
+	this.Data["pageCount"] = pageCount //总页数
+	this.Data["page"] = page           //当前页
 	this.RspTemp("admin/layout.html", "admin/article.html", "c6")
 	return
 }
--- models/article.go
+++ models/article.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"errors"
+	"reflect"
+	"strings"
+
 	"github.com/astaxie/beego/orm"
 )
 
@@ -25,10 +29,12 @@ func (article *Article) Query() orm.QuerySeter {
 	return orm.NewOrm().QueryTable(article)
 }
 
+//注册模型
 func init() {
 	orm.RegisterModel(new(Article))
 }
 
+//新增
 func (article *Article) Insert() error {
 	if _, err := orm.NewOrm().Insert(article); err != nil {
 		return err
@@ -36,6 +42,7 @@ func (article *Article) Insert() error {
 	return nil
 }
 
+//删除
 func (article *Article) Delete() error {
 	if _, err := orm.NewOrm().Delete(article); err != nil {
 		return err
@@ -43,6 +50,7 @@ func (article *Article) Delete() error {
 	return nil
 }
 
+//修改
 func (article *Article) Update(fields ...string) error {
 	if _, err := orm.NewOrm().Update(article, fields...); err != nil {
 		return err
@@ -50,9 +58,81 @@ func (article *Article) Update(fields ...string) error {
 	return nil
 }
 
+//查询
 func (article *Article) Read(fields ...string) error {
 	if err := orm.NewOrm().Read(article, fields...); err != nil {
 		return err
 	}
 	return nil
+}
+
+//分页查询
+func (article *Article) GetList(query map[string]string, fields []string, sortby []string, order []string, page int64, pageSize int64) (ml []interface{}, count int64, err error) {
+	var offset int64
+	if page > 1 {
+		offset = (page - 1) * pageSize
+	}
+	var ad Article
+	qs := ad.Query()
+	for k, v := range query {
+		k = strings.Replace(k, ".", "__", -1) //-1代表全部替换
+		qs = qs.Filter(k, v)
+	}
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			orderby := ""
+			for i, v := range sortby {
+				if strings.ToLower(order[i]) == "asc" {
+					orderby = v
+				} else if strings.ToLower(order[i]) == "desc" {
+					orderby = "-" + v
+				} else {
+					return nil, 0, errors.New("查询参数有误")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) == 1 { //单独一个asc或者dsc
+			orderby := ""
+			for i, v := range sortby {
+				if strings.ToLower(order[i]) == "asc" {
+					orderby = v
+				} else if strings.ToLower(order[i]) == "desc" {
+					orderby = "-" + v
+				} else {
+					return nil, 0, errors.New("查询参数有误")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, 0, errors.New("查询参数有误")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, 0, errors.New("查询参数有误")
+		}
+	}
+	var articles []Article
+	//将切片打散传入
+	qs = qs.OrderBy(sortFields...)
+	if _, err := qs.Limit(pageSize, offset).RelatedSel().All(&articles, fields...); err == nil { //成功
+		if len(fields) == 0 { //查询全部字段
+			for _, v := range articles {
+				ml = append(ml, v)
+			}
+		} else { //根据fields字段进行匹配输出
+			var m map[string]interface{}
+			for _, v := range articles {
+				m = make(map[string]interface{}, 0)
+				val := reflect.ValueOf(v)
+				for _, field := range fields {
+					m[field] = val.FieldByName(field).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		count, _ := qs.Count()
+		return ml, count, nil
+	}
+	return nil, 0, err
 }
